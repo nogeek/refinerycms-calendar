5081c40360eb2bdba685070e3efdf0a93d47224e
Jason Green <jason.green@dynamic50.com>
2011-12-09 13:39:10 +0000

    rails 3.1 changes to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
-Refinery::Application.routes.draw do
-  resources :events do
-    collection do
-      get 'archive/:year(/:month)' => 'events#archive', :as => 'archive'
-      get 'category/:id' => 'event_categories#show', :as => 'category'
+Rails.application.routes.draw do
+  scope(:module => 'refinery') do
+    resources :events do
+      collection do
+        get 'archive/:year(/:month)' => 'events#archive', :as => 'archive'
+        get 'category/:id' => 'event_categories#show', :as => 'category'
+      end
     end
-  end
-  
-  scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
-    resources :events, :except => :show
-    resources :event_categories, :except => :show
+    scope(:path => 'refinery', :as => 'admin', :module => 'admin') do
+      resources :soldiers, :except => :show do
+        post :update_positions, :on => :collection
+      end
+    end
   end
 end
